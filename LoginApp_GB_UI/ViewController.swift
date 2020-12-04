388d6f613e55be44de16c83e3a6bd05470ef6656
yuriyFedyunkin <samnx39@gmail.com>
2020-12-04 23:16:25 +0200

    lesson complete

---
+++ LoginApp_GB_UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp_GB_UI
+//
+//  Created by Yuriy Fedyunkin on 02.12.2020.
+//  Copyright © 2020 Yuriy Fedyunkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
