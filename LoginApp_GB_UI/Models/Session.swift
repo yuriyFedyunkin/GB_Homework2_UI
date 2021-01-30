dbbd500261f10022dd878da442cd6b98c0d7ab43
yuriyFedyunkin <samnx39@gmail.com>
2021-01-30 11:49:53 +0300

    Session singledton added

---
+++ LoginApp_GB_UI/Models/Session.swift
@@ -0,0 +1,20 @@
+//
+//  Session.swift
+//  LoginApp_GB_UI
+//
+//  Created by Yuriy Fedyunkin on 30.01.2021.
+//  Copyright © 2021 Yuriy Fedyunkin. All rights reserved.
+//
+
+import Foundation
+
+struct Session {
+    
+    static var shared = Session()
+    
+    var token: String = ""
+    var userId: Int?
+    
+    private init() {}
+    
+}
