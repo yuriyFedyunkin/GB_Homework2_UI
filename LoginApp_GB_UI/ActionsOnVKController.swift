2d75ae70d89de7c9378b402a7c93468ee771c317
yuriyFedyunkin <samnx39@gmail.com>
2021-01-31 20:13:10 +0300

    base UI + VK authorization

---
+++ LoginApp_GB_UI/ActionsOnVKController.swift
@@ -0,0 +1,35 @@
+//
+//  ActionsOnVKController.swift
+//  LoginApp_GB_UI
+//
+//  Created by Yuriy Fedyunkin on 31.01.2021.
+//  Copyright © 2021 Yuriy Fedyunkin. All rights reserved.
+//
+
+import UIKit
+
+class ActionsOnVKController: UIViewController {
+
+    @IBOutlet weak var searchGroupTF: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func getFriendsPressed(_ sender: Any) {
+    }
+    
+    @IBAction func getPhotosPressed(_ sender: Any) {
+    }
+    
+    @IBAction func getGroupsPressed(_ sender: Any) {
+    }
+    
+    @IBAction func serachGroupByRequest(_ sender: Any) {
+    }
+    
+
+
+}
