a5bc8b2771a29592bbef6804185e8b5048f9359a
yuriyFedyunkin <samnx39@gmail.com>
2021-03-22 22:03:54 +0300

    test

---
+++ LoginApp_GB_UI/Controllers/Cells/PostFeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostFeedCell.swift
+//  LoginApp_GB_UI
+//
+//  Created by Yuriy Fedyunkin on 21.03.2021.
+//  Copyright © 2021 Yuriy Fedyunkin. All rights reserved.
+//
+
+import UIKit
+
+class PostFeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ LoginApp_GB_UI/Models/NewsFeedPostModel.swift
@@ -0,0 +1,9 @@
+//
+//  NewsFeedPostModel.swift
+//  LoginApp_GB_UI
+//
+//  Created by Yuriy Fedyunkin on 22.03.2021.
+//  Copyright © 2021 Yuriy Fedyunkin. All rights reserved.
+//
+
+import Foundation
