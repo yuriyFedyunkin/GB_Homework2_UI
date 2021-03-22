04735b7f9477d21bb925ec542cc1779f2c402c79
yuriyFedyunkin <samnx39@gmail.com>
2021-03-22 23:42:23 +0300

    NewsfeedConroller configured. PostFeedCell configured to retrive parsed info fm VK API

--- LoginApp_GB_UI/Controllers/NewsFeedController.swift
+++ LoginApp_GB_UI/Controllers/NewsFeedController.swift
@@ -10,81 +10,55 @@ import UIKit
 
 class NewsFeedController: UITableViewController {
 
+    private var postFeedList = [NewsfeedPost]()
+    private var source = [Group]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        newsfeedNetworkReqeust()
+        
+        //Градиенти для tableView
+        let gradient = GradientView()
+        gradient.setupGradient(startColor: .blue, endColor: .systemGray, startLocation: 0, endLocation: 1, startPoint: .zero, endPoint: CGPoint(x:0, y: 1))
+        gradient.alpha = 0.6
+        tableView.backgroundView = gradient
+    }
+    
+    // Newsfeed network request
+    private func newsfeedNetworkReqeust() {
+        NetworkManager.shared.getNewsfeedVK { [weak self] posts in
+            DispatchQueue.main.async {
+                self?.postFeedList = posts
+                self?.tableView.reloadData()
+            }
+        } completion2: { [weak self] source in
+            self?.source = source
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return postFeedList.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostFeedCell", for: indexPath) as! PostFeedCell
+        
+        cell.configure(postFeedList[indexPath.row], source)
+        cell.selectionStyle = .none
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- LoginApp_GB_UI/Models/NewsFeedPostModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NewsFeedPostModel.swift
-//  LoginApp_GB_UI
-//
-//  Created by Yuriy Fedyunkin on 22.03.2021.
-//  Copyright © 2021 Yuriy Fedyunkin. All rights reserved.
-//
-
-import Foundation
