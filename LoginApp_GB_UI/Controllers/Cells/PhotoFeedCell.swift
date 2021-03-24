c8f4a1ebda9321a9ba72a1f73e2201889bb0cafc
yuriyFedyunkin <samnx39@gmail.com>
2021-03-24 21:19:21 +0300

    trying to retrive 2 different types of cells

---
+++ LoginApp_GB_UI/Controllers/Cells/PhotoFeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoFeedCell.swift
+//  LoginApp_GB_UI
+//
+//  Created by Yuriy Fedyunkin on 24.03.2021.
+//  Copyright © 2021 Yuriy Fedyunkin. All rights reserved.
+//
+
+import UIKit
+
+class PhotoFeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
